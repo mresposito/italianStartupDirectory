0f5d56b88b7e0045b10611d22c148cdf92a9fcfc
Michele <michele@coursera.org>
2013-07-22 09:02:37 -0700

    added some pages

--- app/org/startupDirectory/pages/Application.scala
+++ app/org/startupDirectory/pages/Application.scala
@@ -6,7 +6,26 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(org.startupDirectory.pages.html.index("Your new application is ready."))
+    Ok(org.startupDirectory.pages.html.index())
   }
+
+  // def persone = Action {
+  //   Ok(org.startupDirectory.pages.html.persone())
+  // }
+
+  // def persone = Action {
+  //   Ok(org.startupDirectory.pages.html.startup())
+  // }
+
+  // def risorse = Action {
+  //   Ok(org.startupDirectory.pages.html.risorse())
+  // }
   
+  def privacy = Action {
+    Ok(org.startupDirectory.pages.html.privacy())
+  }
+
+  def about = Action {
+    Ok(org.startupDirectory.pages.html.about())
+  }
 }
